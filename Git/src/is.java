429de33380a558ea5bd553ef65d84cd3cbd03dfe
itoro <itoro@106-09.info2>
2012-05-11 09:39:07 +0200

    Mi primer Commit

---
+++ Git/src/is.java
@@ -0,0 +1,9 @@
+
+public class is {
+	public static void main (String [] args){
+		int a=0;
+		System.out.println("Hola Jar Jar Binks");
+		if (a==0)
+			System.out.println("Jar Jar Binks ha muerto");
+	}
+}
